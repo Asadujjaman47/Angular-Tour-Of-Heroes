247ebddd2da18e42882d1f4d016bc69e0a139817
Asadujjaman <41225800+Asadujjaman47@users.noreply.github.com>
2023-06-11 17:57:36 +0600

    Add services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,10 +3,12 @@ import { Hero } from '../hero';
 
 import { HEROES } from '../mock-heroes';
 
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
   // hero: Hero = {
@@ -14,10 +16,22 @@ export class HeroesComponent {
   //   name: 'Windstorm'
   // };
 
-  heroes = HEROES;
-  selectedHero ?: Hero;
+  // heroes = HEROES;
+
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 }
